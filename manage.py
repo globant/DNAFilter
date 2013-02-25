10ee89d54063051c4d01c80aa0ede78b4e36cdd3
maxicecilia <maxicecilia@gmail.com>
2013-02-25 14:51:28 -0300

    Changes needed for new settings file handling.

--- manage.py
+++ manage.py
@@ -1,11 +1,18 @@
 #!/usr/bin/python
+'''
+    Changelog:
+    .2013-02-25|mCecilia: Change settings import. Now you have to import the 
+    settings file according to the enviorment where you are working (local, prod, etc).
+    
+'''
 from django.core.management import execute_manager
 try:
-    import settings # Assumed to be in the same directory.
-except ImportError:
+    import settings.local as settings_file # Assumed to be in the same directory.
+except ImportError, e:
     import sys
     sys.stderr.write("Error: Can't find the file 'settings.py' in the directory containing %r. It appears you've customized things.\nYou'll have to run django-admin.py, passing it your settings module.\n(If the file settings.py does indeed exist, it's causing an ImportError somehow.)\n" % __file__)
+    sys.stderr.write("Exception Message: %s" % e.message)
     sys.exit(1)
 
 if __name__ == "__main__":
-    execute_manager(settings)
+    execute_manager(settings_file)
--- urls.py
+++ urls.py
@@ -1,18 +1,15 @@
 import socket
 
 from django.conf.urls.defaults import *
+import settings.common as c
 
 from django.contrib import admin
 admin.autodiscover()
 
-
-production_imgs = '/var/www/dnafilterD/dnafilter/static'
-local_imgs = '/home/sebastian/Projects/dnafilter/dnafilter_d2/dnafilter/static'
-
 urlpatterns = patterns('',
     url(r'^$', 'dnafilter.views.index'),
     url(r'^site_media/(?P<path>.*)$', 'django.views.static.serve',
-    {'document_root': local_imgs if socket.gethostname() == 'glb7765' else production_imgs}),
+    {'document_root': c.STATIC_PATH}),
     url(r'^filter$', 'dnafilter.views.filter'),
     (r'^admin/', include(admin.site.urls)),
 )
